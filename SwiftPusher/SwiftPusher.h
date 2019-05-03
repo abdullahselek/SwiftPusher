0518c4a675782bb758fc25bdc92695fc7cc6e663
Abdullah Selek <abdullahselek@gmail.com>
2019-05-03 17:03:21 +0100

    Create empty project.

---
+++ SwiftPusher/SwiftPusher.h
@@ -0,0 +1,19 @@
+//
+//  SwiftPusher.h
+//  SwiftPusher
+//
+//  Created by Abdullah Selek on 03/05/2019.
+//  Copyright © 2019 Abdullah Selek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftPusher.
+FOUNDATION_EXPORT double SwiftPusherVersionNumber;
+
+//! Project version string for SwiftPusher.
+FOUNDATION_EXPORT const unsigned char SwiftPusherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftPusher/PublicHeader.h>
+
+
