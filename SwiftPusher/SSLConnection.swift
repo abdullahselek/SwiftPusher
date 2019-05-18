75c107eda84f5a686731cb78dad0416a658fc493
Abdullah Selek <abdullahselek@gmail.com>
2019-05-18 21:28:46 +0100

    Start implementing ssl connection.

--- SwiftPusher/SSLConnection.swift
+++ SwiftPusher/SSLConnection.swift
@@ -6,6 +6,26 @@
 //  Copyright © 2019 Abdullah Selek. All rights reserved.
 //
 
-class SSLConnection {
+import Foundation
+
+open class SSLConnection {
+
+    /// The host name, which will be resolved using DNS.
+    fileprivate let host: String
+    /// The host TCP port number.
+    fileprivate let port: Int
+    /// Identity containing a certificate-key pair for setting up the TLS connection.
+    fileprivate let identity: SecIdentity
+    fileprivate let socket: Int
+    fileprivate var context: SSLContext?
+
+    init(withHost host: String,
+         port: Int,
+         identity: SecIdentity) {
+        self.host = host
+        self.port = port
+        self.identity = identity
+        self.socket = -1
+    }
 
 }
---
+++ SwiftPusherTests/SSLConnectionTests.swift
@@ -0,0 +1,15 @@
+//
+//  SSLConnectionTests.swift
+//  SwiftPusherTests
+//
+//  Created by Abdullah Selek on 18/05/2019.
+//  Copyright © 2019 Abdullah Selek. All rights reserved.
+//
+
+import XCTest
+
+@testable import SwiftPusher
+
+class SSLConnectionTests: XCTestCase {
+
+}
