67b869c771555779a63c19a73dc355909b01f89d
Abdullah Selek <abdullahselek@gmail.com>
2019-05-18 17:28:56 +0100

    Create empty sslconnection class.

---
+++ SwiftPusher/SSLConnection.swift
@@ -0,0 +1,11 @@
+//
+//  SSLConnection.swift
+//  SwiftPusher
+//
+//  Created by Abdullah Selek on 18/05/2019.
+//  Copyright © 2019 Abdullah Selek. All rights reserved.
+//
+
+class SSLConnection {
+
+}
